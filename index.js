d6dd297b499183a4ec2c4c708be8042aea46a9c3
Sherry Chen <a00942632@se14l0121-sp12.ad.bcit.ca>
2018-03-16 13:04:49 -0700

    add

---
+++ index.js
@@ -0,0 +1,70 @@
+const port = process.env.PORT || 3003;
+const server = require("http").Server();
+
+var io = require("socket.io")(server);
+
+//var allusers = [];
+//var allusers1 = [];
+//var allusers2 = [];
+
+var allusers = {};
+
+io.on("connection", function(socket){
+    console.log("connect");
+//    allusers.push(socket.id);
+//    console.log(allusers);
+    
+//    socket.emit("yourid", socket.id);
+    
+//    io.emit("user joined", allusers);
+    
+    socket.on("joinroom", function(data){
+        console.log(data);
+        socket.join(data);
+        
+        socket.myRoom = data;
+        socket.emit('yourid', socket.id);
+        
+        if(!allusers[data]){
+            allusers[data] = [];
+        }
+        
+        allusers[data].push(socket.id);
+        io.to(data).emit("userjoined", allusers[data]);
+        
+  /*      if(data == "room1"){
+            
+            allusers1.push(socket.id);
+            io.to(data).emit("userjoined", allusers1);
+            
+        } else if (data == "room2"){
+            
+            allusers2.push(socket.id);
+            io.to(data).emit("userjoined", allusers2);  
+        }*/
+    })
+    
+    socket.on("mymove", function(data){
+//        console.log("data");
+        socket.to(this.myRoom).emit("newmove", data);
+    })
+    
+    socket.on("disconnect", function(){
+       /* var index = allusers.indexOf(socket.id);
+        allusers.splice(index, 1);
+        io.emit("user joined", allusers);*/
+        
+        var index = allusers[this.myRoom].indexOf(socket.id);
+        allusers[this.myRoom].splice(index, 1);
+        io.to(this.myRoom).emit("userjoined", allusers[this.myRoom]);
+    })
+})
+
+server.listen(port,(err)=>{
+    if(err){
+        console.log(err);
+        return false;
+    }
+    
+    console.log("Port is runnning");
+})
